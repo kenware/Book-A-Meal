be1d38233f5abfb8545278fb4a3b625913396982
kenware <ejykken@gmail.com>
2018-04-19 20:24:07 +0100

    finishes feature user should select and order meal from meal menu [Finishes #156877959]

--- ui-template/js/index.js
+++ ui-template/js/index.js
@@ -1,35 +1,58 @@
 
 
 $(window).ready(function(){
-var navpos = $("#myTopnav").offset().top;
-$(window).scroll(function(){
-    //echo("here");
-    var windpos = $(window).scrollTop();
-    if (windpos>navpos){
-    $("#myTopnav").addClass("fixed-top");
-    }else{
-    $("#myTopnav").removeClass("fixed-top");
-    }
- })
 
-
- //dashboard js
+  var navpos = $("#myTopnav").offset().top;
+  $(window).scroll(function(){
+      //echo("here");
+      var windpos = $(window).scrollTop();
+      if (windpos>navpos){
+      $("#myTopnav").addClass("fixed-top");
+      }else{
+      $("#myTopnav").removeClass("fixed-top");
+      }
+   })
+   //dashboard modal js
+   $(".modal-order").hide()
+   $(".remove-modal").click(function(){
+     $(".modal-order").hide();
+   })
+   $(".order").click(function(){
+     $(".modal-order").show()
+   })
+  
+ //dashboard js and admin js
+ $(".timeline-container").hide();//dashboard timeline
  $("#nav2").hide();
  $(".bar1").click(function(){
-  $("#nav2").show();
-  $("#nav1").hide();
+   $("#notification").addClass("bar2");//dashboard timeline
+  $("#notification").removeClass("bar1");//dashboard timeline
+  
+  $("#nav2").show(300000);
+  $("#nav1").hide(300000);
+  $(".timeline-container").show(300000);//dashboard timeline
   
   $("#main").removeClass("main-sidebar1");
   $("#main").addClass("main-sidebar2");
+  
  })
 
  $(".bar2").click(function(){
-  $("#nav2").hide();
-  $("#nav1").show();
+   $("#notification").addClass("bar1");//dashboard timeline
+  $("#notification").removeClass("bar2");//dashboard timeline
+  
+  $("#nav2").hide(300000);
+  $("#nav1").show(300000);
   $("#main").removeClass("main-sidebar2");
   $("#main").addClass("main-sidebar1");
- })
+  $(".timeline-container").hide(300000);
 
+  
+
+ })
+ 
+
+//popover javascript
  $(".p-dash").hide()
  $(".dashboard").hover(function(){
   $(".p-dash").show()
@@ -68,6 +91,14 @@ $(window).scroll(function(){
  })
  $(".logout").mouseleave(function(){
   $(".p-out").hide()
+ })
+
+ $(".p-notif").hide()
+ $(".notif").hover(function(){
+  $(".p-notif").show()
+ })
+ $(".notif").mouseleave(function(){
+  $(".p-notif").hide()
  })
 
 })
