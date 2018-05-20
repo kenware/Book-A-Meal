dee741f59562d8813db9053268fa228e1cc4eae9
kenware <ejykken@gmail.com>
2018-05-20 15:55:03 +0100

    finishes feature user should get notification

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+
 import volleyball from 'volleyball';
 import userRoute from './server/route/user';
 import mealRoute from './server/route/meal';
 import menuRoute from './server/route/menu';
 import orderRoute from './server/route/order';
+
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,6 +22,7 @@ app.use('/api/v1', userRoute);
 app.use('/api/v1', menuRoute);
 app.use('/api/v1', mealRoute);
 app.use('/api/v1', orderRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './client/index.html')));
--- client/components/header/index.js
+++ client/components/header/index.js
@@ -1,9 +1,10 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.scss';
 // import { Link } from 'react-router-dom';
 const nav = () => (
   <div className="nav" id="myTopnav">
-    <a href="index.html" className="active" >Book-A-Meal</a>
+    <a><Link to="/meals" className="active" >Book-A-Meal</Link></a>
     <a href="dashboard.html">Dashboard</a>
     <a href="admin.html">Admin</a>
     <a href="admin-set-menu.html">Set-Menu</a>
